900f26dff6d713c43048e849cfc0ea54b4615dd1
abderrahmanl <lahiaouni.abd@gmail.com>
2023-06-03 10:40:23 +0000

    added code

---
+++ wp-content/themes/astra/header.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The header for Astra Theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?><!DOCTYPE html>
+<?php astra_html_before(); ?>
+<html <?php language_attributes(); ?>>
+<head>
+<?php astra_head_top(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php 
+if ( apply_filters( 'astra_header_profile_gmpg_link', true ) ) {
+	?>
+	 <link rel="profile" href="https://gmpg.org/xfn/11"> 
+	 <?php
+} 
+?>
+<?php wp_head(); ?>
+<?php astra_head_bottom(); ?>
+</head>
+
+<body <?php astra_schema_body(); ?> <?php body_class(); ?>>
+<?php astra_body_top(); ?>
+<?php wp_body_open(); ?>
+
+<a
+	class="skip-link screen-reader-text"
+	href="#content"
+	role="link"
+	title="<?php echo esc_attr( astra_default_strings( 'string-header-skip-link', false ) ); ?>">
+		<?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?>
+</a>
+
+<div
+<?php
+	echo astra_attr(
+		'site',
+		array(
+			'id'    => 'page',
+			'class' => 'hfeed site',
+		)
+	);
+	?>
+>
+	<?php
+	astra_header_before();
+
+	astra_header();
+
+	astra_header_after();
+
+	astra_content_before();
+	?>
+	<div id="content" class="site-content">
+		<div class="ast-container">
+		<?php astra_content_top(); ?>
+
+<script>
+	var scroll = window.pageYOffset || document.documentElement.scrollTop;
+	var navHeight = document.querySelector('.site-header').offsetHeight;
+	
+	window.addEventListener("scroll", function(event) { 
+		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		if (scrolled > navHeight) {
+			document.querySelector('.site-header').classList.add('animate');
+		} else {
+			document.querySelector('.site-header').classList.remove('animate');
+		}
+
+		if (scrolled > scroll) {
+			document.querySelector('.site-header').classList.remove('sticky');
+		} else {
+			document.querySelector('.site-header').classList.add('sticky');
+		}
+		
+		scroll = window.pageYOffset || document.documentElement.scrollTop;
+	});				
+</script>
